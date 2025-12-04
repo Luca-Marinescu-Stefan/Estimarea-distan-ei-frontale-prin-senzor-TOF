980dcb394fa43c12a4ec5c1571b86c7009f0dc5d
Luca-Marinescu-Stefan <luca.marinescu@stud.fiir.upb.ro>
2025-12-04 09:28:09 +0200

    Update train_model.py

--- src/neural_network/train_model.py
+++ src/neural_network/train_model.py
@@ -1,1 +1,14 @@
+# Modelul Neuronal (MLP)
+import tensorflow as tf
 
+def creare_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(64, activation='relu', input_shape=(3,)),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(optimizer='adam', loss='mse')
+    print("Modelul MLP a fost creat.")
+    return model
+
+if __name__ == "__main__":
+    creare_model()
